11ee3714039ad41850244bb587fa0ad1ab3d2f46
Jérémy Piorun <jpiorun@insitaction.com>
2023-02-03 00:58:33 +0100

    feat: ajout du front single movie

--- views/movies/single.php
+++ views/movies/single.php
@@ -1,62 +1,74 @@
 <section class="movie">
     <div class="container-medium">
-        <h1><?= $movie->title ?></h1>
+        <div class="movie__banner-container">
+            <div class="movie__banner-item">
+                <div class="backdropmask"></div>
+                <?php echo "<img src='$movie->banner' alt='' /><br />"; ?>
+            </div>
+        </div>
+        <div class="movie__infos">
+            <div class="movie__infos-item">
+                <?php echo "<img src='$movie->film_poster' alt='' /><br />"; ?>
+                <div>
+                <?php
+                    foreach ($actors as $actor) {
+                        echo $actor['name'] . "<br />";
+                        echo $actor['character'] . "<br />";
+                        echo $actor['picture'] . "<br />";
+                        echo "<hr />";
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="movie__infos-item">
+                <h1 class="title"><?= $movie->title ?></h1>
+                <p>Date de sortie: <?php $date = IntlCalendar::fromDateTime($movie->released_at, "fr_FR");
+                echo IntlDateFormatter::formatObject($date, IntlDateFormatter::RELATIVE_SHORT); ?></p>
+                <div class="movie__infos-item-summary">
+                    <?php echo $movie->summary ?>
+                </div>
+                <div class="movie__infos-item-trailer">
+                    <?php echo "<iframe width='560' height='315' src='$movie->trailer'allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>" ?>
+                </div>
+                <div class="movie__infos-item-gallery">
+                    <?php
+                    foreach ($gallery as $item) {
+                        echo "<img class='movie__picture' src='$item->picture' alt='Image du film' /><br />";
+                    }
+                    ?>
+                </div>
+            </div>
 
-        <?php
-        echo $movie->summary . '<br />';
-        echo "<img src='$movie->banner' alt='' /><br />";
-        echo $movie->trailer . "<br />";
-        $date = IntlCalendar::fromDateTime($movie->released_at, "fr_FR");
-        echo IntlDateFormatter::formatObject($date, IntlDateFormatter::TRADITIONAL);
-        ?>
+            <!-- <?php if ($is_login) { ?>
+                <h3>Formulaire</h3>
 
-        <h2>Galerie d'images</h2>
+                <form action="<?= '../../comments/add/'; ?>" method="post">
+                    <textarea name="message"></textarea>
+                    <input type="hidden" name="movie_id" value=<?= $movie->id ?> />
+                    <input type="submit" value="Envoyer" />
+                </form>
+            <?php } ?>
 
-        <div class="movie__gallery">
+            <h3>Liste des commentaires</h3>
+
             <?php
-            foreach ($gallery as $item) {
-                echo "<img class='movie__picture' src='$item->picture' alt='Image du film' /><br />";
+            foreach ($comments as $comment) {
+                echo $comment->username . "<br />";
+                echo $comment->created_at . "<br />";
+                echo $comment->content . "<br />";
+                echo "<hr />";
+
+                if ($comment->user_id == $user_id) {
+                    echo "<a href='/comments/{$comment->comment_id}/edit/movies/{$movie->id}/'>Éditer</a>";
+                    echo "<a href='/comments/{$comment->comment_id}/delete/movies/{$movie->id}/'>Supprimer</a>";
+                }
             }
             ?>
+
+            <h2>Liste des acteurs</h2>
+
+             -->
+            </div>
         </div>
-
-        <h2>Espace commentaire</h2>
-
-        <?php if ($is_login) { ?>
-            <h3>Formulaire</h3>
-
-            <form action="<?= '../../comments/add/'; ?>" method="post">
-                <textarea name="message"></textarea>
-                <input type="hidden" name="movie_id" value=<?= $movie->id ?> />
-                <input type="submit" value="Envoyer" />
-            </form>
-        <?php } ?>
-
-        <h3>Liste des commentaires</h3>
-
-        <?php
-        foreach ($comments as $comment) {
-            echo $comment->username . "<br />";
-            echo $comment->created_at . "<br />";
-            echo $comment->content . "<br />";
-            echo "<hr />";
-
-            if ($comment->user_id == $user_id) {
-                echo "<a href='/comments/{$comment->comment_id}/edit/movies/{$movie->id}/'>Éditer</a>";
-                echo "<a href='/comments/{$comment->comment_id}/delete/movies/{$movie->id}/'>Supprimer</a>";
-            }
-        }
-        ?>
-
-        <h2>Liste des acteurs</h2>
-
-        <?php
-        foreach ($actors as $actor) {
-            echo $actor['name'] . "<br />";
-            echo $actor['character'] . "<br />";
-            echo $actor['picture'] . "<br />";
-            echo "<hr />";
-        }
-        ?>
-    </div>
+        
 </section>
