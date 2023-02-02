89ab3852418b902dcc9963ca3574c56f9c643a22
Nathan Dubocage <nathandubocage@gmail.com>
2023-02-03 00:17:57 +0100

    Ajout de certains trucs d'administration

--- views/actors/actors.php
+++ views/actors/actors.php
@@ -1,24 +1,38 @@
 <section class="actors">
     <div class="container-medium">
-        <h1>Liste des acteurs avec les films</h1>
+        <h1 class="actors__heading">Le casting</h1>
 
         <?php
         if ($is_admin) {
-            echo "<a href='/actors/add/'>Ajouter un acteur</a>";
+            echo "<button class='button-container button--small mt-20'><a href='/actors/add/'>Ajouter un acteur</a></button>";
         }
-        foreach ($actorsAll as $name => $actors) {
-            echo "<h2>{$name}</h2>";
-            foreach ($actors as $index => $actor) {
-                echo "{$actor['title']} - ";
-                echo "{$actor['character']}";
-                if ($index == 0 && $is_admin) {
-                    echo "<a href='/actors/{$actor['actor_id']}/edit/'>Modifier cet acteur</a> ";
-                    echo "<a href='/actors/{$actor['actor_id']}/delete/'>Supprimer cet acteur</a>";
-                }
-                echo "<hr />";
-            }
-        }
-        ?>
 
+        foreach ($actorsAll as $name => $actors) {
+        ?>
+            <h2 class="actors__name"><?= $name; ?></h2>
+
+            <div class="actors__movies">
+        <?php
+            foreach ($actors as $index => $actor) {
+        ?>
+                <div class="actors__container">
+                <p class="actors__identity"><?= $actor['title']; ?> - <?= $actor['character']; ?></p>
+        <?php
+                if ($index == 0 && $is_admin) {
+                    echo "<div>";
+                    echo "<a class='movies__link movies__link--admin mr-10' href='/actors/{$actor['actor_id']}/edit/'>Modifier cet acteur</a> ";
+                    echo "<a class='movies__link movies__link--admin' href='/actors/{$actor['actor_id']}/delete/'>Supprimer cet acteur</a>";
+                    echo "</div>";
+                }
+        ?>
+                </div>
+        <?php
+            }
+        ?>
+            </div>
+            <img class="actors__image" src="<?= $actor['picture']; ?>" />
+        <?php
+            }
+        ?>
     </div>
 </section>
--- views/actors/add.php
+++ views/actors/add.php
@@ -1,15 +1,17 @@
-<h1>Ajouter un acteur</h1>
+<section class="add-actor">
+    <h1 class="add-title">Ajouter un acteur</h1>
 
-<form method="post" action="#">
-    <input type="text" name="name" placeholder="Nom de l'acteur" /> <br />
-    <input type="text" name="character" placeholder="Personnage joué par cet acteur" /> <br />
-    <input type="text" name="picture" placeholder="Image" /> <br />
+    <form method="post" action="#">
+        <input class="input-container" type="text" name="name" placeholder="Nom de l'acteur" /> <br />
+        <input class="input-container mt-10" type="text" name="character" placeholder="Personnage joué par cet acteur" /> <br />
+        <input class="input-container mt-10" type="text" name="picture" placeholder="Image" /> <br />
 
-    <h2>Associer à des films</h2>
+        <h2 class="mt-10">Associer à des films</h2>
 
-    <?php foreach ($movies as $movie) { ?>
-        <input id="<?= $movie->id ?>" type="checkbox" name="<?= $movie->id ?>" /> <label for="<?= $movie->id ?>"><?= $movie->title; ?></label> <br />
-    <?php } ?>
+        <?php foreach ($movies as $movie) { ?>
+            <input class="mt-10 checkbox-container"  id="<?= $movie->id ?>" type="checkbox" name="<?= $movie->id ?>" /> <label class="ml-5 mt-u-10" for="<?= $movie->id ?>"><?= $movie->title; ?></label> <br />
+        <?php } ?>
 
-    <input type="submit" name="add" value="Ajouter" />
-</form>
+        <input class="button-container mt-20" type="submit" name="add" value="Ajouter" />
+    </form>
+</section>
